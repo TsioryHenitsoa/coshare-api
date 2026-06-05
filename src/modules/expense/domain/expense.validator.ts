ca469e73cf28447d4004b17af442c1f3fd0ce55a
TsioryHenitsoa <tsioryhents@gmail.com>
2026-06-05 16:28:48 +0400

    Cleaning architecture

--- src/modules/expense/domain/expense.validator.ts
+++
@@ -1,7 +0,0 @@
-export class ExpenseValidator {
-    static validateAmount(amount: number){
-        if (amount <= 0) {
-            throw new Error('Amount must be greater than zero');
-        }
-    }
-}
